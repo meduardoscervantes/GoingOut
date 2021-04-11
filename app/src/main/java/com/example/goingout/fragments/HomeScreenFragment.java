25ec26e23024ee618bf64894bbd8ba348289e1e5
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-04-11 05:18:08 -0500

    Added the HomeScreen

---
+++ app/src/main/java/com/example/goingout/fragments/HomeScreenFragment.java
@@ -0,0 +1,41 @@
+package com.example.goingout.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.goingout.R;
+
+public class HomeScreenFragment extends Fragment {
+
+    public HomeScreenFragment() {
+        // Required empty public constructor
+    }
+    public static HomeScreenFragment newInstance() {
+        HomeScreenFragment fragment = new HomeScreenFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+    }
+}
